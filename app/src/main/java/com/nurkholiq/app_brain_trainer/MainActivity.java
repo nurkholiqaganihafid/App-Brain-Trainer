62b0227dedc325ccc3ff78b3fc4d7549b21356d0
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2022-04-22 20:50:09 +0700

    make a summation game

--- app/src/main/java/com/nurkholiq/app_brain_trainer/MainActivity.java
+++ app/src/main/java/com/nurkholiq/app_brain_trainer/MainActivity.java
@@ -1,14 +1,120 @@
 package com.nurkholiq.app_brain_trainer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.nurkholiq.app_brain_trainer.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    ArrayList<Integer> answers = new ArrayList<>();
+    int locationOfCorrectAnswer;
+    int score = 0;
+    int numberOfQuestions = 0;
+
+    public void playAgain(View view) {
+        score = 0;
+        numberOfQuestions = 0;
+        binding.tvTimer.setText("30s");
+        binding.tvPoints.setText("0/0");
+        binding.tvResult.setText("");
+        binding.btnPlayAgain.setVisibility(View.INVISIBLE);
+
+        generateQuestion();
+
+        new CountDownTimer(30100, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                binding.tvTimer.setText(String.valueOf(millisUntilFinished / 1000) + "s");
+            }
+
+            @Override
+            public void onFinish() {
+                binding.btnPlayAgain.setVisibility(View.VISIBLE);
+                binding.tvTimer.setText("0s");
+                binding.tvResult.setText("Your score " + Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+            }
+        }.start();
+    }
+
+    public void generateQuestion() {
+        Random rand = new Random();
+
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        binding.tvSum.setText(Integer.toString(a) + " + " + Integer.toString(b) + " = ?");
+
+        locationOfCorrectAnswer = rand.nextInt(4);
+
+        answers.clear();
+
+        int incorrectAnswer;
+
+        for (int i = 0; i < 4; i++) {
+            if (i == locationOfCorrectAnswer) {
+                answers.add(a + b);
+            } else {
+                incorrectAnswer = rand.nextInt(41);
+
+                while (incorrectAnswer == a + b) {
+                    incorrectAnswer = rand.nextInt(41);
+                }
+                answers.add(incorrectAnswer);
+            }
+        }
+
+        binding.btnA.setText(Integer.toString(answers.get(0)));
+        binding.btnB.setText(Integer.toString(answers.get(1)));
+        binding.btnC.setText(Integer.toString(answers.get(2)));
+        binding.btnD.setText(Integer.toString(answers.get(3)));
+    }
+
+    public void chooseAnswer(View view) {
+        if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+            score++;
+            binding.tvResult.setText("Benar!");
+        } else {
+            binding.tvResult.setText("Salah!");
+        }
+
+        numberOfQuestions++;
+        binding.tvPoints.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+        generateQuestion();
+    }
+
+    public void start(View view) {
+        binding.btnStart.setVisibility(View.INVISIBLE);
+        binding.tvTitle.setVisibility(View.INVISIBLE);
+        binding.btnNo.setVisibility(View.INVISIBLE);
+        binding.rlGame.setVisibility(RelativeLayout.VISIBLE);
+        playAgain(binding.btnPlayAgain);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        binding.btnNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
